b64fb168b80c45257785bf71d29b9a0e267b9ac5
Hampton Reese <hampton@spacee.com>
2021-11-22 13:40:07 -0600

    DM-393 adding alerts for specific test cases

--- orchestrator/orchestrator.js
+++ orchestrator/orchestrator.js
@@ -69,12 +69,6 @@ async function getDNN(drone) {
 }
 
 
-// Filter function for array distint values
-function _distinct(value, index, self) {
-  return self.indexOf(value) === index;
-}
-
-
 // Helper function to set environment variables per test suite execution, hopefully making these a little more discoverable
 // Note that some of these variable names are dynamic based on the drone type: ROVR, OBSERVR
 function _setEnvironmentVariables(drone) {
@@ -134,21 +128,78 @@ async function runDroneTest(drone) {
       });
     }
   });
-  // report.failures = failures.filter(_distinct);
   
   return report;
 }
 
 
-// Send slack alert to channel based on environment variable
-async function sendSlackAlert(report) {
-  if(!SlackWebHookUrl) { 
-    console.log('SLACK_WEBHOOK_URL is not configured');
-    return; 
+// Search drone failures for a partular test case.  If any drones failed the test case, construct the alert message
+function _getAlertByFailedTest(report, severity, testMatch) {
+  testMatch = testMatch.toLowerCase();
+  let failedDrones = [];
+
+  report.drones.forEach(drone => {
+    // don't bother looking if drone was skipped or if all tests were passed
+    if (!drone.isTested || drone.totalFailedTests == 0) { return; }
+
+    console.log(`EVALUATING DRONE ${drone.dsn}`);
+    let failedTest = false;
+    for(suite in drone.failures) {
+      let cnt = drone.failures[suite].filter(tst => { return tst.toLowerCase().indexOf(testMatch) !== -1; }).length;
+      if(cnt) { failedTest = true; }
+    }
+
+    console.log(`FAILED TEST: ${failedTest}`);
+
+    if(failedTest) {
+      failedDrones.push(drone.dsn);
+    }
+  });
+
+  console.log(`FOUND ${failedDrones.length} drones that failed test ${testMatch}`);
+  
+  if(failedDrones.length > 0) {
+    let alert = {
+      severity: severity,
+      message: `The following ${failedDrones.length} drones failed the test '${testMatch}':\n`
+    };
+    failedDrones.forEach(dsn => alert.message += `\n - ${dsn}`);
+    return alert;
   }
 
-  let evaluation = _evaluateAlert(report);
-  if(!evaluation.isAlarm) { return; }
+  return null;
+}
+
+
+// The rules for sending alerts, right now this is just a boolean response but
+// this could be changed to an object to determine serverity, etc
+// Seperating this into it's own function because it may get rather large/complex as we build the rules
+function _evaluateReport(report) {
+  let alerts = [];
+
+  // alert if any drone are skipped, this can be addressed the next day
+  if (report.summary.skippedDrones > 0) {
+    let alert = {
+      severity: 'P3',
+      message: `The following ${report.summary.skippedDrones} drones were skipped due to not having an assigned DNN:\n`
+    };
+    report.drones.filter(d => !d.dnn).forEach(d => alert.message += `\n - ${d.dsn}`);
+    alerts.push(alert);
+  }
+
+  let temperatureAlert = _getAlertByFailedTest(report, 'P3', 'Temperature value should be lower then 85');
+  if(temperatureAlert) {
+    alerts.push(temperatureAlert);
+  }
+
+  return alerts;
+}
+
+
+// Send slack alert to channel based on environment variable
+async function sendSlackAlert(webhookUrl, report) {
+  let alerts = _evaluateReport(report);
+  if(!alerts.length) { return; }
 
   let body = {
     blocks: [
@@ -168,56 +219,25 @@ async function sendSlackAlert(report) {
           },
           {
             type: "mrkdwn",
-            text: `*Total Tests:*\n${report.summary.totalDroneTests}`
-          },
-          {
-            type: "mrkdwn",
-            text: `*Total Failures:*\n${report.summary.totalFailedTests}`
+            text: `*Passed Tests:*\n${report.summary.totalPassedTests}/${report.summary.totalDroneTests}`
           }
         ]
       },
-      {
-        type: "section",
-        fields: [
-          {
-            type: "mrkdwn",
-            text: `*${evaluation.severity} Alert*:\n*${evaluation.message}`
-          }
-        ]
-      }
     ]
   };
 
-  await axios.post(SlackWebHookUrl, body);
-}
+  alerts.forEach(alrt => 
+    body.blocks.push({
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*${alrt.severity} Alert*:\n${alrt.message}`
+      }
+    })
+  );
 
-// The rules for sending alerts, right now this is just a boolean response but
-// this could be changed to an object to determine serverity, etc
-// Seperating this into it's own function because it may get rather large/complex as we build the rules
-function _evaluateAlert(report) {
-  // alert when skipping more than 10% of assigned drones
-  let skippedPct = report.summary.skippedDrones / report.summary.totalDrones;
-  if (skippedPct > .1) {
-    return {
-      isAlarm: true,
-      severity: 'P3',
-      message: `${Math.round(skippedPct * 100)}% of drones skipped testing.  This happens when no DNN has been assigned.`
-    };
-  }
-
-  // alert when more than 10% of drone tests failed
-  let droneTestFailurePct = report.summary.totalFailedTests / report.summary.totalDroneTests;
-  if (droneTestFailurePct > .1) {
-    return {
-      isAlarm: true,
-      severity: 'P3',
-      message: `${Math.round(droneTestFailurePct * 100)}% of drone tests failed.`
-    };
-  }
-
-  return {
-    isAlarm: false
-  };
+  console.log(JSON.stringify(body));
+  await axios.post(webhookUrl, body);
 }
 
 
@@ -237,6 +257,7 @@ function _evaluateAlert(report) {
     drones: []
   };
 
+  let start = Date.now();
   for (let key in drones) {
     let drone = drones[key];
 
@@ -259,17 +280,24 @@ function _evaluateAlert(report) {
     }
     report.drones.push(droneTestReport);
   }
+  let end = Date.now();
+  report.summary.durationSeconds = Math.round((end - start) / 100) / 10;
 
   console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
   console.log('########################################################################################');
   console.info(report.summary);
-  report.drones.forEach(x => console.log(x));
+  report.drones.forEach(x => {
+    console.log(x);
+  });
   console.log('########################################################################################');
   console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+  // console.log(JSON.stringify(report));
 
-  await sendSlackAlert(report);
+
+  if(!SlackWebHookUrl) { 
+    console.log('SLACK_WEBHOOK_URL is not configured');
+    return; 
+  }
+
+  await sendSlackAlert(SlackWebHookUrl, report);
 })();
-
-  // FACING ID ???
-  // EVALUATE RESULTS
-  // SEND SLACK MESSAGE
