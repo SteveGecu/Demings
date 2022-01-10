4055d9f83afe5e890bb195c34d7be974b85abb37
festarr <felix@spacee.com>
2022-01-10 10:26:32 -0600

    Update URL

--- Deming/Polaris/playwright.config.js
+++ Deming/Polaris/playwright.config.js
@@ -4,14 +4,14 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
     use: {
-      baseURL: 'https://polaris-ui.azurewebsites.net',
+      baseURL: 'https://polaris-ui.deming.qa.eastus2.spacee.io/login',
       //screenshot: 'only-on-failure',
       //trace: 'retain-on-failure',
     },
 
     timeout: 30000,
     //retries: 2,
-    //globalSetup: require.resolve('./global-setup'),
+    globalSetup: require.resolve('./global-setup'),
   };
   
   module.exports = config;
